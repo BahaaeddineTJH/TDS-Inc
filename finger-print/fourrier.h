77b64f2220b4b10a00a7a4fd002cd18579f2fa37
theo.loret <theo@theo-VirtualBox.(none)>
2022-03-13 16:32:18 +0100

    rm blabla

--- finger-print/fourrier.h
+++
@@ -1,9 +0,0 @@
-#include <complex.h>
-#include <fftw3.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sox.h>
-
-double* get_data(char* file,int* n);
-
-void fourrier_transform(double* in, fftw_complex* out, int n);
