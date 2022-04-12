8bc9816e1fd65f2327cf1030e17b0efcd34b8feb
Théo Loret <Theo@Air-de-admin>
2022-04-12 16:19:09 +0200

    .

--- local-server/client.c
+++ local-server/client.c
@@ -7,8 +7,40 @@
 #include <netdb.h>
 #include <err.h>
 
+#include "../finger-print/finger_print.h"
+
+#define BUFFER_SIZE 256
+
+void rewrite(int fd, void* buf, size_t len){
+    size_t index = 0;
+    while(index < len){
+        ssize_t r = write(fd,buf+index,len-index);
+        if(r == -1)
+            errx(1,"Could not write into fd");
+        len += r;
+    }
+}
+
+long* wrap_get_hash(char* path,size_t* len){
+    sox_format_init();
+    printf("Hello\n");
+    songinfo s_info;
+    double* in = get_data(path,&s_info);
+    printf("Hello\n");
+    fftw_complex* tab = create_tab(in, &s_info);
+    free(in);
+    printf("Hello\n");
+    long* p = hash_tab(tab, &s_info);
+    free(tab);
+    printf("Hello\n");
+    *len = s_info.duration;
+
+    sox_format_quit();
+    return p;
+}
+
 int main(int argc, char** argv) {
-    if (argc != 3)
+    if (argc != 4)
         errx(EXIT_FAILURE, "Usage:\n"
                 "Arg 1 = ip address (e.g. 127.0.0.1)\n"
                 "Arg 2 = Port number (e.g. 2048)\n");
@@ -38,8 +70,39 @@ int main(int argc, char** argv) {
     if(cnx == -1)
         errx(1,"Connection failed.");
     printf("Connected to the server.\n");
-    /*
-     *  send request
-     */
+
+    //send song hash to server
+    size_t len;
+    long* hash = wrap_get_hash(argv[4],&len);
+    printf("Hash Done.\n");
+    write(cnx,hash,len*sizeof(long));
+    printf("Hash sent\n");
+    free(hash);
+
+    printf("Waiting for answer...\n");
+
+    //get answer from server
+    char* answer = NULL;
+    char buffer[BUFFER_SIZE];
+    size_t answer_len = 0;
+    ssize_t r;
+    while ((r = read(cnx,buffer,BUFFER_SIZE)) != 0){
+        if(r == -1)
+            errx(1,"Could not read from fd");
+        answer = realloc(answer,answer_len+r);
+        for (ssize_t i = 0; i < r; ++i) {
+            answer[len+i] = buffer[i];
+        }
+        len += r;
+    }
+    answer = realloc(answer,answer_len+1);
+    answer[answer_len] = '\0';
+    printf("the answer is : %s\n",answer);
+
+
+    free(answer);
+
+    close(cnx);
+
     return 0;
 }
